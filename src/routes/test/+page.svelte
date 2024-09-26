84a87801d86fed776bc955456c57e0e633ee5271
IOnlyNeedParenthesis <>
2024-09-26 10:40:39 +0200

    effect and untrack

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { untrack } from "svelte";
+
+    let value = $state('');
+
+    let firstRun = $state(true);
+    $effect(() => {
+        console.log('effect')
+        if (untrack(() => firstRun)) {
+            firstRun = false;
+            console.log("First run");
+        } else {
+            console.log("Not first run");
+            console.log(value)
+        }
+    });
+</script>
+
+<p>{value}</p>
+<input bind:value type="text" name="" id="">
